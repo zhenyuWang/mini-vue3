b6ffe31ef53df94f8e00ffebefa1df35b989c8b5
zhenyuWang <13641039885@163.com>
2022-03-26 01:34:44 +0800

    feat: effect stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,31 +1,55 @@
-const targetsMap = new Map();
+import {extend} from '../../src/shared'
+
+// map 收集所有响应式数据 根据响应式数据的 key 收集所有的依赖
+const targetsMap = new Map()
 
 // 接收当前 effect 实例
-let activeEffect;
+let activeEffect
 
 class ReactiveEffect {
-  private _fn;
-  constructor(fn,public scheduler?){
+  private _fn
+  public scheduler:Function | undefined
+  deps = new Set()
+  active = true
+  onStop?:Function
+  constructor(fn,scheduler?:Function){
     this._fn = fn
+    this.scheduler = scheduler
   }
   run(){
-    activeEffect = this;
-    return this._fn();
+    activeEffect = this
+    return this._fn()
   }
+  stop(){
+    if(this.active){
+      cleanupEffect(this)
+      if(this.onStop){
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+function cleanupEffect(effect){
+  effect.deps.forEach((dep:any) => {
+    dep.delete(effect)
+  })
 }
 
 export function track(target,key){
   let depsMap = targetsMap.get(target)
   if(depsMap === undefined){
-    depsMap = new Map();
+    depsMap = new Map()
     targetsMap.set(target,depsMap)
   }
   let dep = depsMap.get(key)
   if(dep===undefined){
-    dep = new Set();
+    dep = new Set()
     depsMap.set(key,dep)
   }
+  if(activeEffect===undefined) return
   dep.add(activeEffect)
+  activeEffect.deps.add(dep)
 }
 
 export function trigger(target,key){
@@ -36,14 +60,23 @@ export function trigger(target,key){
       effect.scheduler()
     }
     else{
-      effect.run();
+      effect.run()
     }
   }
 }
 
 export function effect(fn,option?){
-  const _effect = new ReactiveEffect(fn,option?.scheduler);
-  _effect.run();
+  const _effect = new ReactiveEffect(fn,option?.scheduler)
+  // 将 option 上的东西都给到 _effect
+  extend(_effect,option)
 
-  return _effect.run.bind(_effect);
+  _effect.run()
+  const runner:any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
+}
+
+export function stop(runner){
+  runner.effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,19 +1,21 @@
 import {reactive} from '../reactive'
-import {effect} from '../effect'
+import {effect,stop} from '../effect'
 
 describe('effect',() => {
+
   it('happy path',() => {
     const user = reactive({
       age:10
     })
-    let nextAge;
+    let nextAge
     effect(() => {
       nextAge = user.age+1
     })
     expect(nextAge).toBe(11)
-    user.age++;
+    user.age++
     expect(nextAge).toBe(12)
   })
+
   it('should return runner when call effect',() => {
     // effect(fn) 会返回 runner
     // 执行 runner 会执行 fn，并且会返回 fn 的返回值
@@ -27,6 +29,7 @@ describe('effect',() => {
     expect(num).toBe(12)
     expect(res).toBe('test runner')
   })
+
   it('scheduler',() => {
     // 1. 通过 effect 的第二个参数传递一个对象，对象中包含 scheduler
     // 2. effect 初始化调用 第一个参数 fn
@@ -54,5 +57,38 @@ describe('effect',() => {
     expect(dummy).toBe(1)
     run()
     expect(dummy).toBe(2)
+  })
+
+  it('stop',() => {
+    // 可以通过 stop 解除依赖
+    let dummy
+    const obj = reactive({prop:1})
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    runner()
+    expect(dummy).toBe(3)
+  })
+
+  it('onStop',() => {
+    // onStop => stop 的 回调函数
+    const obj = reactive({foo:1})
+    const onStop = jest.fn()
+    let dummy
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },{
+        onStop
+      }
+    )
+    stop(runner)
+    expect(onStop).toBeCalledTimes(1)
   })
 })
