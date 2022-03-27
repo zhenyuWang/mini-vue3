cbd9e6b4d955aa4b32b81955fe407aa010150564
zhenyuWang <13641039885@163.com>
2022-03-28 00:32:49 +0800

    feat: init element

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,14 @@ import { h } from '../../lib/guide-mini-vue.esm.js'
 
 export const App = {
   render() {
-    return h('div', `hi ${this.msg}`)
+    // children => string
+    // return h('div', { id: 'root', class: ['c_red'] }, `hi mini-vue3`)
+    // children => array
+    return h('div', { id: 'root', class: 'title c_red' }, [
+      `hi mini-vue3`,
+      h('p', { class: 'c_blue' }, 'this is child div'),
+      h('p', { class: 'c_green' }, 'this is child p')
+    ])
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject, isString } from '../shared/index'
 import {createComponentInstance,setupComponent} from './component'
 
 export function render(vnode,container){
@@ -5,8 +6,70 @@ export function render(vnode,container){
 }
 
 function patch(vnode,container){
-  // 处理 Component
-  processComponent(vnode,container)
+  // 处理 text
+  if(isString(vnode)){
+    processText(vnode,container)
+  }
+  else if(isString(vnode.type)){
+    // 处理 element
+    processElement(vnode,container)
+  }
+  else if(isObject(vnode.type)){
+    // 处理 Component
+    processComponent(vnode,container)
+  }
+}
+
+function processText(vnode,container){
+  container.textContent = vnode
+}
+
+function processElement(vnode,container){
+  mountElement(vnode,container)
+}
+
+function mountElement(vnode,container){
+  const el = document.createElement(vnode.type)
+
+  // handle children
+  const {children} = vnode
+  if(isString(children)){
+    el.textContent = children
+  }else if(Array.isArray(children)){
+    mountChildren(vnode,el)
+  }
+
+  // handle props
+  const {props} = vnode
+  if (props) {
+    for (const key in props) {
+      patchProp(
+        el,
+        key,
+        props[key]
+      )
+    }
+  }
+
+  container.append(el)
+}
+
+function mountChildren(vnode,container){
+  vnode.children.forEach(child => {
+    patch(child,container)
+  })
+}
+
+function patchProp(el,key,value){
+  if (key === 'innerHTML' || key === 'textContent') {
+    el[key] = value == null ? '' : value
+  }
+
+  if (value === '' || value == null) {
+    el[key] = ''
+    el.removeAttribute(key)
+  }
+  el.setAttribute(key,value)
 }
 
 function processComponent(vnode,container){
