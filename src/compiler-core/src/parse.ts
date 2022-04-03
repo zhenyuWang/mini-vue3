a1bcea299a20c17f484529018f5e5e9d0f611a7c
zhenyuWang <13641039885@163.com>
2022-04-03 12:56:16 +0800

    feat: parse element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import {NodeTypes} from './ast'
 
+const enum TagType {
+  Start,
+  End
+}
+
 export function baseParse(content:string){
   const context = createParserContext(content)
 
@@ -11,8 +16,13 @@ function parseChildren(context){
   const nodes:any[] = []
 
   let node
-  if(context.source.startsWith('{{')){
+  const s = context.source
+  if(s.startsWith('{{')){
     node = parseInterpolation(context)
+  }else if(s[0]==='<'){
+    if(/[a-z]/i.test(s[1])){
+      node = parseElement(context)
+    }
   }
 
   nodes.push(node)
@@ -20,7 +30,7 @@ function parseChildren(context){
   return nodes
 }
 
-function parseInterpolation(context){
+function parseInterpolation(context: any){
   // {{message}}
 
   const openDelimiter = '{{'
@@ -42,6 +52,27 @@ function parseInterpolation(context){
       type:NodeTypes.SIMPLE_EXPRESSION,
       content
     }
+  }
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context,TagType.Start)
+
+  parseTag(context,TagType.End)
+
+  return element
+}
+
+function parseTag(context:any,type:TagType){
+  const match:any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  advanceBy(context,match[0].length+1)
+
+  if(type === TagType.End) return
+
+  return {
+    type:NodeTypes.ELEMENT,
+    tag
   }
 }
 
